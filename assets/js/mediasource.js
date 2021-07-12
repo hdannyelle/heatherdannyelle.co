6d3c2767afa73ad71c01ec28f6ec8d35d875402f
hdannyelle <hhdthompson@gmail.com>
2021-07-12 21:25:11 +0200

    updated resume page

--- assets/js/mediasource.js
+++ assets/js/mediasource.js
@@ -1,20 +1,3 @@
-
-// -- Create a MediaSource and attach it to the video (We already learned about that) --
-
-const videoTag = document.getElementById("my-video");
-const myMediaSource = new MediaSource();
-const url = URL.createObjectURL(myMediaSource);
-videoTag.src = url;
-
-// 1. add source buffers
-
-const videoSourceBuffer = myMediaSource
-  .addSourceBuffer('video/mp4; codecs="avc1.64001e"');
-
-// the same for the video SourceBuffer
-fetch("assets/video/hero_one_web.mp4").then(function(response) {
-  // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
-}).then(function(videoData) {
-  videoSourceBuffer.appendBuffer(videoData);
-});
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
